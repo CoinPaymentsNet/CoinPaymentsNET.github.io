dfccce7c28bcb67710f0433b7d46efc46c54ac5c
USS-Worker <andrew.zotov@ukrainiansoftware.com>
2020-11-09 10:21:07 +0200

    Little update  with html tags.

--- add-API-section.js
+++ add-API-section.js
@@ -13,12 +13,13 @@ setTimeout(function () {
     var tutorialsHtml = `  <div class="container-fluid">
     <div class="row ml-1" style="margin-top: 30px;">
         <div class="col-md-4">
-      <h1>Authentication</h1>
+      <h1>Authentication </h1>
+      <h3>Old version</h3>
      <p>The old API uses a SHA-512 HMAC signature generated with your private key. But the new one uses a HmacSHA-256 instead. Our server generates it's own HMAC signature and compares it with the API caller's. If they don't match the API call is discarded. The HMAC signature then encodes with Base64 schemes and is sent as a HTTP header called 'X-CoinPayments-Signature'.
 Also you should add two new headers. One of them is '"X-CoinPayments-Timestamp' with current date converting to a string, according to universal time. Another header is called 'X-CoinPayments-Client' preserving information about your clientId.
-The HMAC signature is created from the request method, request url, clientId, date and request body. For example if your API secret key was "test" and public key was "your_api_public_key" (both without quotes) and you would like to send request to /api/v1/merchant/callbacks the text for generating HMAC might look like:</p>
+The HMAC signature is created from the request method, request url, clientId, date and request body. For example if your API secret key was <b>"test"</b> and public key was <b>"your_api_public_key"</b> (both without quotes) and you would like to send request to <b>/api/v1/merchant/callbacks</b> the text for generating HMAC might look like:</p>
         <code>
-        GEThttps://orion-api.starhermit.com/api/v1/merchant/callbacks7aa5e7ba45d84d978c5ea7f62498abf4Mon, 02 Nov 2020 12:16:38 GMT
+        GET https://orion-api.starhermit.com/api/v1/merchant/callbacks7aa5e7ba45d84d978c5ea7f62498abf4Mon, 02 Nov 2020 12:16:38 GMT
         </code>
         </br>
         </br>
@@ -27,6 +28,7 @@ The HMAC signature is created from the request method, request url, clientId, da
         Gpv0yNiv4zWHukg5peVCPcYTCSVzH6t9QFLzF2Rq49E=
         </code>
         <h1>Requests example</h1>
+        <h4>Old Version</h4>
         <p>For getting callback using old API you have to send <strong>HTTP</strong> request(<strong>POST</strong>) <strong>https://www.coinpayments.net/api.php/</strong>
         with such a x-www-form-urlencoded body:
         <ul>
@@ -36,6 +38,7 @@ The HMAC signature is created from the request method, request url, clientId, da
         <li>currency: BTC</li>
         
         </ul>
+        <h4>New version</h4>
         <p>For list all callback addresses sorted descending by the creation date using new API we'll send  <strong>HTTP</strong> request(<strong>GET</strong>) to <strong>/api/v1/merchant/callbacks</strong> with parameters:</p>
         <ul>
           <li>clientId - The merchant client id whose callback address should be listed</li>
